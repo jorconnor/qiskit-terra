70ed1c674029584daa8428877c2b55b28c07cb03
Jordan Connor <jordanhconnor@gmail.com>
2021-05-11 00:19:12 -0400

    Removes stestrs grouping configuration.
    Removing  the grouping regex in .stestr.conf should allow stestr to pack its test workers better, and therefore improve the speed of test execution. However, pre-existing test cases utilizing non-unique tempfiles began to fail after the configuration change. This commit modifies such tests to pass under these new conditions. This also allows the test suite to properly execute using pytest-xdist.
    
    Fixed #6105

--- test/python/test_user_config.py
+++ test/python/test_user_config.py
@@ -21,135 +21,59 @@ from qiskit import user_config
 
 class TestUserConfig(QiskitTestCase):
 
-    @classmethod
-    def setUpClass(cls):
-        cls.file_path = 'temp.txt'
+    def setUp(self):
+        super().setUp()
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        test_name = str(self.id()).split('.')[-1]
+        file_path = os.path.join(current_dir, 'configurations', test_name + ".conf")
+        self.config = user_config.UserConfig(file_path)
 
     def test_empty_file_read(self):
-        config = user_config.UserConfig(self.file_path)
-        config.read_config_file()
+        self.config.read_config_file()
         self.assertEqual({},
-                         config.settings)
+                         self.config.settings)
 
     def test_invalid_optimization_level(self):
-        test_config = """
-        [default]
-        transpile_optimization_level = 76
-        """
-        self.addCleanup(os.remove, self.file_path)
-        with open(self.file_path, 'w') as file:
-            file.write(test_config)
-            file.flush()
-            config = user_config.UserConfig(self.file_path)
-            self.assertRaises(exceptions.QiskitUserConfigError,
-                              config.read_config_file)
+        self.assertRaises(exceptions.QiskitUserConfigError,
+                          self.config.read_config_file)
 
     def test_invalid_circuit_drawer(self):
-        test_config = """
-        [default]
-        circuit_drawer = MSPaint
-        """
-        self.addCleanup(os.remove, self.file_path)
-        with open(self.file_path, 'w') as file:
-            file.write(test_config)
-            file.flush()
-            config = user_config.UserConfig(self.file_path)
-            self.assertRaises(exceptions.QiskitUserConfigError,
-                              config.read_config_file)
+        self.assertRaises(exceptions.QiskitUserConfigError,
+                          self.config.read_config_file)
 
     def test_circuit_drawer_valid(self):
-        test_config = """
-        [default]
-        circuit_drawer = latex
-        """
-        self.addCleanup(os.remove, self.file_path)
-        with open(self.file_path, 'w') as file:
-            file.write(test_config)
-            file.flush()
-            config = user_config.UserConfig(self.file_path)
-            config.read_config_file()
-            self.assertEqual({'circuit_drawer': 'latex'},
-                             config.settings)
+        self.config.read_config_file()
+        self.assertEqual({'circuit_drawer': 'latex'},
+                         self.config.settings)
 
     def test_optimization_level_valid(self):
-        test_config = """
-        [default]
-        transpile_optimization_level = 1
-        """
-        self.addCleanup(os.remove, self.file_path)
-        with open(self.file_path, 'w') as file:
-            file.write(test_config)
-            file.flush()
-            config = user_config.UserConfig(self.file_path)
-            config.read_config_file()
-            self.assertEqual(
-                {'transpile_optimization_level': 1},
-                config.settings)
+        self.config.read_config_file()
+        self.assertEqual(
+            {'transpile_optimization_level': 1},
+            self.config.settings)
 
     def test_invalid_num_processes(self):
-        test_config = """
-        [default]
-        num_processes = -256
-        """
-        self.addCleanup(os.remove, self.file_path)
-        with open(self.file_path, 'w') as file:
-            file.write(test_config)
-            file.flush()
-            config = user_config.UserConfig(self.file_path)
-            self.assertRaises(exceptions.QiskitUserConfigError,
-                              config.read_config_file)
+        self.assertRaises(exceptions.QiskitUserConfigError,
+                          self.config.read_config_file)
 
     def test_valid_num_processes(self):
-        test_config = """
-        [default]
-        num_processes = 31
-        """
-        self.addCleanup(os.remove, self.file_path)
-        with open(self.file_path, 'w') as file:
-            file.write(test_config)
-            file.flush()
-            config = user_config.UserConfig(self.file_path)
-            config.read_config_file()
-            self.assertEqual(
-                {'num_processes': 31},
-                config.settings)
+        self.config.read_config_file()
+        self.assertEqual(
+            {'num_processes': 31},
+            self.config.settings)
 
     def test_valid_parallel(self):
-        test_config = """
-        [default]
-        parallel = False
-        """
-        self.addCleanup(os.remove, self.file_path)
-        with open(self.file_path, 'w') as file:
-            file.write(test_config)
-            file.flush()
-            config = user_config.UserConfig(self.file_path)
-            config.read_config_file()
-            self.assertEqual(
-                {'parallel_enabled': False},
-                config.settings)
+        self.config.read_config_file()
+        self.assertEqual(
+            {'parallel_enabled': False},
+            self.config.settings)
 
     def test_all_options_valid(self):
-        test_config = """
-        [default]
-        circuit_drawer = latex
-        circuit_mpl_style = default
-        circuit_mpl_style_path = ~:~/.qiskit
-        transpile_optimization_level = 3
-        suppress_packaging_warnings = true
-        parallel = false
-        num_processes = 15
-        """
-        self.addCleanup(os.remove, self.file_path)
-        with open(self.file_path, 'w') as file:
-            file.write(test_config)
-            file.flush()
-            config = user_config.UserConfig(self.file_path)
-            config.read_config_file()
-            self.assertEqual({'circuit_drawer': 'latex',
-                              'circuit_mpl_style': 'default',
-                              'circuit_mpl_style_path': ['~', '~/.qiskit'],
-                              'transpile_optimization_level': 3,
-                              'num_processes': 15,
-                              'parallel_enabled': False},
-                             config.settings)
+        self.config.read_config_file()
+        self.assertEqual({'circuit_drawer': 'latex',
+                          'circuit_mpl_style': 'default',
+                          'circuit_mpl_style_path': ['~', '~/.qiskit'],
+                          'transpile_optimization_level': 3,
+                          'num_processes': 15,
+                          'parallel_enabled': False},
+                         self.config.settings)
--- test/python/visualization/test_gate_map.py
+++ test/python/visualization/test_gate_map.py
@@ -12,8 +12,9 @@
 
 """A test for visualizing device coupling maps"""
 import unittest
-import os
 
+from io import BytesIO
+from PIL import Image
 from ddt import ddt, data
 from qiskit.test.mock import FakeProvider
 from qiskit.test import QiskitTestCase
@@ -25,6 +26,13 @@ from .visualization import path_to_diagram_reference, QiskitVisualizationTestCas
 
 if HAS_MATPLOTLIB:
     import matplotlib.pyplot as plt
+
+
+def _write_plot_to_image(fig):
+    img_buffer = BytesIO()
+    fig.savefig(img_buffer, format='png')
+    img_buffer.seek(0)
+    return Image.open(img_buffer)
 
 
 @ddt
@@ -41,11 +49,10 @@ class TestGateMap(QiskitVisualizationTestCase):
         """ tests plotting of gate map of a device (20 qubit, 16 qubit, 14 qubit and 5 qubit)"""
         n = backend.configuration().n_qubits
         img_ref = path_to_diagram_reference(str(n) + "bit_quantum_computer.png")
-        filename = "temp.png"
         fig = plot_gate_map(backend)
-        fig.savefig(filename)
-        self.assertImagesAreEqual(filename, img_ref, 0.2)
-        os.remove(filename)
+        result_img = _write_plot_to_image(fig)
+        self.assertImagesAreEqual(result_img, img_ref, 0.2)
+        plt.close(fig)
 
     @data(*backends)
     @unittest.skipIf(not HAS_MATPLOTLIB, 'matplotlib not available.')
@@ -58,11 +65,10 @@ class TestGateMap(QiskitVisualizationTestCase):
         circuit._layout.add_register(qr)
         n = backend.configuration().n_qubits
         img_ref = path_to_diagram_reference(str(n) + "_plot_circuit_layout.png")
-        filename = str(n) + "_plot_circuit_layout_result.png"
         fig = plot_circuit_layout(circuit, backend)
-        fig.savefig(filename)
-        self.assertImagesAreEqual(filename, img_ref, 0.1)
-        os.remove(filename)
+        result_img = _write_plot_to_image(fig)
+        self.assertImagesAreEqual(result_img, img_ref, 0.1)
+        plt.close(fig)
 
 
 @unittest.skipIf(not HAS_MATPLOTLIB, 'matplotlib not available.')
